ahumay/dd

commit 523b01f776928080e99457a53ee5841ff4c13e2a

API: add attribute `cudd.Function.var`
made possible by d8517a5dbed4e49eeacb32af7ad9c043af3aca16.
Similar to existing attributes of class `autoref.Function`.

dd/cudd.pyx
@@ -1331,6 +1331,11 @@ cdef class Function(object):
         return index
 
     @property
+    def var(self):
+        """Variable at level where this node is."""
+        return self.bdd._var_with_index[self.index]
+
+    @property
     def level(self):
         """Level where this node currently is."""
         i = self.index

commit b391159f73971d6b7822ca4a7ce6b9500cd7b128

BUG: return `None` from `cudd.Function.var` if node is constant
introduced in:  523b01f776928080e99457a53ee5841ff4c13e2a

dd/cudd.pyx
@@ -1395,7 +1395,12 @@ cdef class Function(object):
 
     @property
     def var(self):
-        """Variable at level where this node is."""
+        """Variable at level where this node is.
+
+        If node is constant, return `None`.
+        """
+        if Cudd_IsConstant(self.node):
+            return None
         return self.bdd._var_with_index[self._index]
 
     @property

tests/cudd_test.py
@@ -695,6 +695,7 @@ def test_function():
     assert low == bdd.false, low
     high = x.high
     assert high == bdd.true, high
+    assert x.var == 'x', x.var
     # ~ x
     not_x = ~x
     assert not_x.negated
@@ -702,6 +703,12 @@ def test_function():
     assert low == bdd.false, low
     high = not_x.high
     assert high == bdd.true, high
+    assert not_x.var == 'x', not_x.var
+    # constant nodes
+    false = bdd.false
+    assert false.var is None, false.var
+    true = bdd.true
+    assert true.var is None, true.var
 
 
 def test_function_support():